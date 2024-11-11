5fccc215715995e02e6a164b8e3a5f371ee5b2af
Mario Duran-Vega <57765660+MarioDuran@users.noreply.github.com>
2024-11-11 11:35:05 -0600

    update train validation

--- train.py
+++ train.py
@@ -600,6 +600,7 @@ def train(hyp, opt, device, callbacks):
                         plots=plots,
                         callbacks=callbacks,
                         compute_loss=compute_loss,
+                        video_test_len = opt.video_test_len
                     )  # val best model with plots
                     if is_coco:
                         callbacks.run("on_fit_epoch_end", list(mloss) + list(results) + lr, epoch, best_fitness, fi)
